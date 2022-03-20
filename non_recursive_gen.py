88b27305c4cf3894c2ed34115c4badb2778fd93c
Kevin Bowen <kevin.bowen@gmail.com>
2022-03-20 09:47:17 -0700

    Add non-recursive generator example

---
+++ non_recursive_gen.py
@@ -0,0 +1,54 @@
+"""
+A binary tree class example taken from PEP 255 - Simple Generators
+source: https://peps.python.org/pep-0255/
+"""
+
+
+class Tree:
+    def __init__(self, label, left=None, right=None):
+        self.label = label
+        self.left = left
+        self.right = right
+
+    def __repr__(self, level=0, indent="    "):
+        s = level*indent + 'self.label'
+        if self.left:
+            s = s + "\n" + self.left.__repr__(level+1, indent)
+        if self.right:
+            s = s + "\n" + self.right.__repr__(level+1, indent)
+        return s
+
+    def __iter__(self):
+        return inorder(self)
+
+
+# Create a Tree from a list.
+def tree(list):
+    n = len(list)
+    if n == 0:
+        return []
+    i = int(n / 2)
+    return Tree(list[i], tree(list[:i]), tree(list[i+1:]))
+
+
+# A non-recursive generator that generated Tree labels in in-order.
+def inorder(node):
+    stack = []
+    while node:
+        while node.left:
+            stack.append(node)
+            node = node.left
+        yield node.label
+        while not node.right:
+            try:
+                node = stack.pop()
+            except IndexError:
+                return
+            yield node.label
+        node = node.right
+
+# Create a tree
+t = tree("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+print("Print the nodes of the tree in in-order.")
+for x in t:
+    print(x)
